96953909a918a7f7127d2a3a695ed9cdfad26c3d
Marcus Brose <contact@marcusbrose.com>
2023-06-19 17:42:22 +0200

    add mysql

--- storefront/settings.py
+++ storefront/settings.py
@@ -84,9 +84,16 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'admin123',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
